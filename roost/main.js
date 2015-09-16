91df8130ec9c76454b6e65d14ae2935ec0aa3585
Johnson <jwpage@gmail.com>
2015-09-16 22:37:45 +1000

    Initial commit

---
+++ roost/main.js
@@ -0,0 +1,20 @@
+(function() {
+    var latLng = new google.maps.LatLng(-32.92692, 151.78138);
+    var mapOptions = {
+        zoom: 15,
+        center: latLng,
+        styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#ec4646"},{"visibility":"on"}]}],
+        scrollwheel: false
+    };
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: 'Snazzy!'
+    });
+})();
---
+++ roost/members.js
@@ -0,0 +1,98 @@
+var roostMembers = [
+    {
+        "name": "Sean Bell",
+        "title": "Graphic Designer / #TDKtuesday Newcastle Host",
+        "url": "http://essbeecreative.com.au"
+    },
+    {
+        "name": "Benjamin Mitchell",
+        "title": "Illustrator",
+        "url": "http://www.benmitchell.com.au"
+    },
+    {
+        "name": "Bec Dujin",
+        "title": "Marketing Strategist & Creative Consultant",
+        "url": "http://www.talktobec.com.au"
+    },
+    {
+        "name": "Adrian Garner",
+        "title": "Independent multimedia artist",
+        "url": "http://adrian.com.au"
+    },
+    {
+        "name": "Chris Stonestreet",
+        "title": "Creative Director",
+        "url": "http://thefluid.com.au"
+    },
+    {
+        "name": "Mark Owens",
+        "title": "Graphic Designer & Illustrator",
+        "url": "http://www.bananabolt.com"
+    },
+    {
+        "name": "David Kidston",
+        "title": "Architect",
+        "url": "http://www.hackarchitecture.com.au/"
+    },
+    {
+        "name": "Stephen Babic",
+        "title": "Graphic Designer",
+        "url": "http://missingmouse.com/"
+    },
+    {
+        "name": "Justin Spaull",
+        "title": "Photographer",
+        "url": "http://www.justinaaron.com.au/"
+    },
+    {
+        "name": "Perlin Earth",
+        "title": "Traveller",
+        "url": "http://perlinsadventureblog.com/"
+    },
+    {
+        "name": "Mitch Lee",
+        "title": "Portrait Photographer",
+        "url": "http://mitchleephoto.com/"
+    },
+    {
+        "name": "Rachael Ainsworth",
+        "title": "App Developer"
+    },
+    {
+        "name": "Mark Tisdell",
+        "title": "Landscape Design / Architecture Design / Urban Design / mudesign.com.au"
+    },
+    {
+        "name": "Johnson Page",
+        "title": "Web Application Developer"
+    },
+    {
+        "name": "Michael McCallum",
+        "title": "Arts Marketing",
+        "url": " http://artsmarketingaustralia.com.au"
+    },
+    {
+        "name": "Andy Jones",
+        "title": "Video Production",
+        "url": " andy.jones93@gmail.com"
+    },
+    {
+        "name": "Stephen Roberts",
+        "title": "Video Producer",
+        "url": " http://www.beautifulmotionpictures.com"
+    },
+    {
+        "name": "Robert Preston",
+        "title": "Games Developer"
+    },
+    {
+        "name": "Brett McDonald",
+        "title": "Graphic Designer",
+        "url": " www.brettmcdonald.com.au"
+    },
+    {
+        "name": "Alexandra Morris",
+        "title": " Content Creator",
+        "url": "freelance writer http://www.coffeecavewoman.com"
+    }
+];

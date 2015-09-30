5b6542743a9370bdb95547e8a7b7c20807942f32
Johnson <jwpage@gmail.com>
2015-09-30 20:48:24 +1000

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+    $    = require('gulp-load-plugins')()
+    ;
+
+gulp.task('less', function() {
+    return gulp.src('less/color.less')
+        .pipe($.less())
+        .pipe(gulp.dest('build'))
+        ;
+});
+
+gulp.task('css', ['less'], function() {
+    var input = [
+            'bootstrap/css/bootstrap.css',
+            'fonts/fonts.css',
+            'stylesheets/hiding-nav.css',
+            'stylesheets/menu-transition.css',
+            'stylesheets/parallax-layers.css',
+            'stylesheets/home-06.css',
+            'stylesheets/slimmenu.css',
+            'stylesheets/venobox.css',
+            'stylesheets/main.css',
+            'stylesheets/main-responsive.css',
+            'stylesheets/main-retina.css',
+            'build/color.css',
+            'roost/style.css'
+        ];
+
+    return gulp.src(input)
+        .pipe($.concat('style.css'))
+        .pipe($.minifyCss())
+        .pipe(gulp.dest('build'))
+        ;
+});
+
+gulp.task('js', function () {
+    var input = [
+        'bootstrap/js/jquery.js',
+        'javascripts/jquery.easing.1.3.js',
+        'bootstrap/js/bootstrap.js',
+        'javascripts/device.min.js',
+        'javascripts/modernizr.js',
+        'javascripts/retina.js',
+        'javascripts/responsive-nav.js',
+        'javascripts/navi-slidedown.js',
+        'javascripts/equalheights.js',
+        'javascripts/jquery.parallax-1.1.3.js',
+        'javascripts/jquery.tweet.js',
+        'javascripts/jquery.slimmenu.min.js',
+        'javascripts/venobox.min.js',
+        'javascripts/venobox-init.js',
+        'javascripts/equalheights-init.js',
+        'javascripts/hiding-nav.js',
+        'javascripts/parallax-init.js',
+        'javascripts/main.js',
+        'roost/members.js',
+        'roost/ejs.min.js',
+        'roost/main.js',
+    ];
+    var output = 'build';
+
+    return gulp.src(input)
+        .pipe($.concat('main.js', {newLine: ';'}))
+        .pipe(gulp.dest(output))
+        ;
+});
+
+gulp.task('default', ['less', 'css']);
